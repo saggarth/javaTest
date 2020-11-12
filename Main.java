677fd04ead9da84f058de78bec88825533feb3f2
Denis Molchanov <62193982+saggarth@users.noreply.github.com>
2020-11-12 22:49:35 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,13 @@ public class Main {
     public static void main(String[] args) {
         long unixSeconds = 1605188168;
         Date date = new java.util.Date(unixSeconds*1000L);
-        SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss z");
-        sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+3"));
-        String formattedDate = sdf.format(date);
-        System.out.println(formattedDate);
+        String[] patterns = {"dd-MM-yyyy HH:mm:ss z", "yyyy", "MM", "dd"};
+        //SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss z");
+        //sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+3"));
+        //String formattedDate = sdf.format(date);
+        //System.out.println(formattedDate);
+        for (String pattern : patterns) {
+            System.out.println(new SimpleDateFormat(pattern).format(date));
+        }
     }
 }
