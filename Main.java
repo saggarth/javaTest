8f7cc79e74d89cce490488a4ab7eba9e1b722f3e
Denis Molchanov <62193982+saggarth@users.noreply.github.com>
2020-11-13 12:28:40 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,20 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         long unixSeconds = 1605188168;
-        Date date = new java.util.Date(unixSeconds*1000L);
-        String[] patterns = {"dd-MM-yyyy HH:mm:ss z", "yyyy", "MM", "dd"};
-        //SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss z");
-        //sdf.setTimeZone(java.util.TimeZone.getTimeZone("GMT+3"));
-        //String formattedDate = sdf.format(date);
-        //System.out.println(formattedDate);
-        for (String pattern : patterns) {
-            System.out.println(new SimpleDateFormat(pattern).format(date));
+        System.out.println(dateConv("f", unixSeconds));
+        System.out.println(dateConv("y", unixSeconds));
+        System.out.println(dateConv("m", unixSeconds));
+        System.out.println(dateConv("d", unixSeconds));
+    }
+
+    static String dateConv (String k, long timestamp) {
+        Date date = new Date(timestamp * 1000L);
+        switch (k) {
+            case "f": return new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z").format(date); //возвращает полное значение даты и времени
+            case "y": return new SimpleDateFormat("yyyy").format(date); //возвращает год
+            case "m": return new SimpleDateFormat("MM").format(date); //возвращает месяц
+            case "d": return new SimpleDateFormat("dd").format(date); //возвращает день
         }
+        return null;
     }
 }
